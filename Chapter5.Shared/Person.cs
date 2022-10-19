c9da54ecc0c5f1ecee1e89223aa8bcb11f67a389
Regnaroz <k-0-1@outlook.com>
2022-10-19 20:12:56 +0300

    -chpater 5

---
+++ Chapter5.Shared/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5.Shared
+{
+    public class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+
+        public Person(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public (string, DateTime) GetData()
+        {
+            return (PersonName:this.Name, PersonDate : this.DateOfBirth);
+    }
+    }
+
+   
+}
---
+++ chapter5/Program.cs
@@ -0,0 +1,8 @@
+﻿using Chapter5.Shared;
+
+var person = new Person("Khalid",DateTime.UtcNow);
+var personData = person.GetData();
+(person.Name,person.DateOfBirth) = personData;
+var name1 = personData.Item1;
+var dateOfBorth = personData.Item2;
+Console.WriteLine(personData);
